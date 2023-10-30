1195ab925facb107250b3031e37cbe245bc1d40d
Adam Sims <adamsims1203@gmail.com>
2023-10-30 18:15:28 -0500

    View records

---
+++ pages/admin/records/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Records</h1>
+    <records-table-list-records :records="records" />
+  </div>
+</template>
+<script setup>
+definePageMeta({ layout: "admin" });
+const supabase = useSupabaseClient();
+const config = useRuntimeConfig();
+
+const records = ref([]);
+
+onMounted(async () => {
+  let { data, error } = await supabase
+    .from("records")
+    .select(
+      "*, codes: code_id(*,rel_users_to_organizations: link_id(*, profiles: profile_id(*)) ) "
+    );
+
+  records.value = data;
+  console.log(data);
+});
+</script>
+<style scoped></style>
